0a5887d483f6607e2316b6f388cbf54d1dec8ddc
Oleg Nikitashin <olegnikitashin@evrone.com>
2018-12-14 10:57:59 +0300

    Add cap roles

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
-server '89.223.95.252', user: 'rails_api_user'
+# server '89.223.95.252', user: 'rails_api_user'
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server '89.223.95.252', user: 'rails_api_user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
